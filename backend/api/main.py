482651644c8e7aa5bf9773fbdfa23de39c28516b
SottoJunRabin <gunaid1231@outlook.jp>
2026-04-17 20:31:17 +0900

    branch作成

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,24 +1,7 @@
-from fastapi import FastAPI,Query
-from typing import Annotated
-from pydantic import BaseModel
-from typing import Union
+from fastapi import FastAPI
 
 app = FastAPI()
-items = ["tシャツ","スカート","ブーツ","コート"]
 
-# データの取得
-@app.get("/items")
-def read_items(skip: int = 0,limit: Annotated[int,Query(ge=1, le=10)]= 10):
-  return {"items": items[skip : skip + limit]}
-
-
-class Item(BaseModel):
-  name: str
-  price: float
-  description: Union[str, None] = None
-
-# データの登録
-@app.post("/items/")
-def create_item(item: Item):
-  print(f"データを登録します:{item.name},{item.price},{item.description}")
-  return item
+@app.get("/") # httpメソッドのgetでルートの一番メインのurlにアクセスがあったら、下の関数を処理する
+async def index():
+  return {"message":"Hello World"}
